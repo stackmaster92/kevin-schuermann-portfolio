daf022d0ddda7839a27e5c1ebbd5e856de374aad
Takahiro92 <takahiro.hiroshi.swe92@gmail.com>
2025-01-14 18:16:14 +0900

    update working history

--- pages/api/recommendations.js
+++ pages/api/recommendations.js
@@ -5,7 +5,7 @@ const recommendationCard = [
     name: "Prosa.ai",
     image: "images/logo.png",
     designation: "Senior Full-Stack Developer",
-    view: "I integrated NLP, chatbot solutions, and text recognition to enhance user interaction and automate systems. I migrated legacy systems to a microservices architecture, improving scalability and reliability. I worked closely with product and UX teams to design intuitive dashboards and real-time features. Additionally, I implemented Web3.js for Solidity smart contract integration, enabling decentralized applications and seamless blockchain interactions.",
+    view: "I engineered a chatbot and text recognition system using Python, TensorFlow, and NLTK to enhance user engagement and automate processes. I transitioned a monolithic legacy system into a microservices architecture with Docker, Kubernetes, and Node.js, improving scalability and reliability. Additionally, I designed and implemented real-time dashboards and user interfaces with React, Redux, and TypeScript to provide seamless user experiences. My work also involved integrating blockchain technology, leveraging Web3.js and Ether.js to enable secure and decentralized smart contract interactions. Collaborating cross-functionally, I defined project roadmaps, optimized feature delivery, and ensured code quality through comprehensive testing with Jest and Cypress.",
     linkednURL: "https://www.linkedin.com/in/muhammadullahafridi/",
   },
   {
@@ -13,7 +13,7 @@ const recommendationCard = [
     name: "Overjet",
     image: "images/logo.png",
     designation: "Senior Software Engineer",
-    view: "I led the development of high-performance applications, focusing on scalability and security using Node.js, React, and TypeScript. I migrated legacy systems to a microservices architecture, improving reliability and scalability. I integrated Google Cloud Platform (GCP) services for streamlined deployment, monitoring, and scalability. I enhanced user experience through close collaboration with product and UX teams, designing intuitive dashboards and real-time features. Additionally, I mentored junior developers, fostering innovation and encouraging best practices throughout the team.",
+    view: "I directed technical development for complex projects, leveraging Next.js, Nest.js, GraphQL, Apollo, Tailwind CSS, AWS, and PostgreSQL to deliver high-quality solutions. I enhanced system infrastructure by implementing Terraform, improving scalability, automation, and maintainability. Additionally, I established standardized CI/CD pipelines to accelerate deployment processes and ensure reliability. By building robust APIs with Nest.js and developing reusable UI components using React and TypeScript, I supported a wide range of product features, ensuring efficiency and consistency across projects.",
     linkednURL: "https://www.linkedin.com/in/muhammadullahafridi/",
   },
   {
@@ -21,7 +21,7 @@ const recommendationCard = [
     name: "FlairsTech",
     image: "images/logo.png",
     designation: "FullStack Developer",
-    view: "I developed responsive web applications with React, managed state using Redux-Saga, and styled interfaces with Tailwind CSS. On the backend, I built RESTful APIs using Python and Django, integrated Firebase for real-time data, and used Amazon RDS and GraphQL for scalable database operations. I also implemented WebSocket protocols for real-time updates and improved system architecture to ensure scalability and reliability.",
+    view: "I developed responsive web applications using React, Redux, and Redux-Saga, delivering efficient state management and optimized user interfaces. I built RESTful APIs with Python and Django, integrating Firebase for real-time data synchronization and seamless user authentication. To enhance backend performance, I optimized databases with Amazon RDS, implemented GraphQL for flexible data querying, and incorporated WebSocket for real-time functionality. Collaborating with cross-functional teams, I refined system architecture to ensure scalability and robust performance under high traffic conditions.",
     linkednURL: "https://www.linkedin.com/in/muhammadullahafridi/",
   },
   {
@@ -29,7 +29,7 @@ const recommendationCard = [
     name: "Chattermill",
     image: "images/logo.png",
     designation: "Lead Backend Developer",
-    view: "I led backend development for Chattermill’s ride-hailing platform, improving real-time dispatch systems using Node.js and Express.js. I designed and implemented RESTful APIs to enable seamless communication between core services and external partners, integrated PostgreSQL and MongoDB to support high-volume transactions and real-time updates, and implemented CI/CD pipelines to automate deployments, reducing downtime and enhancing reliability. Additionally, I collaborated with cross-functional teams to optimize system architecture and boost performance during peak loads.",
+    view: "I spearheaded backend development for a ride-hailing platform, optimizing real-time dispatch systems with Node.js and Express.js. I designed and deployed RESTful APIs to enable seamless communication between core services and external partners, while enhancing data handling and system performance by integrating PostgreSQL and MongoDB for high-volume transactions. By establishing CI/CD pipelines, I automated deployments, reducing downtime and improving operational reliability. Collaborating closely with stakeholders, I improved system architecture to ensure peak performance during high-load scenarios.",
     linkednURL: "https://www.linkedin.com/in/nasirkhan22/",
   },
   {
@@ -37,7 +37,7 @@ const recommendationCard = [
     name: "Tourlane",
     image: "images/logo.png",
     designation: "Frontend Developer",
-    view: "I developed dynamic, responsive web applications using React.js to build interactive UIs, managing state with Redux and handling asynchronous actions using Redux-Saga. I designed pixel-perfect, responsive layouts with Tailwind CSS to ensure optimal user experience across devices, and implemented reusable, modular UI components for efficient code management and scalability. I followed best practices in frontend development to optimize performance and ensure smooth user interactions, while utilizing Git for version control to streamline collaboration and code management.",
+    view: "I developed dynamic and interactive web applications using React.js, creating intuitive user interfaces while managing application state with Redux and implementing Redux-Saga for efficient asynchronous operations. I designed pixel-perfect, responsive layouts with Tailwind CSS, ensuring a consistent user experience across all devices, and built reusable, modular UI components to enhance code maintainability and scalability. By adhering to industry best practices, I optimized user interactions and application performance for seamless functionality.",
     linkednURL: "https://www.linkedin.com/in/sikandar-hayat-381407179/",
   },
   {
@@ -45,7 +45,7 @@ const recommendationCard = [
     name: "Ephemeral",
     image: "images/logo.png",
     designation: "Frontend developer",
-    view: "I developed dynamic, responsive web applications using React.js and Next.js for the frontend and built scalable backend systems using Python, Flask, and Django. I designed and implemented RESTful APIs to ensure smooth communication between frontend and backend and integrated WebSocket servers for real-time features, enhancing user interactions. I utilized SQL databases like PostgreSQL and MySQL, as well as NoSQL databases like MongoDB and Redis, for efficient data management. Additionally, I deployed applications using AWS, Google Cloud, and Azure, leveraging Docker and Kubernetes for scalability. I ensured high-quality code through unit testing with Jest and managed version control using Git.",
+    view: "I developed highly responsive web applications using React.js and Next.js to deliver scalable and dynamic frontends, while engineering backend systems with PHP and Laravel to ensure robust functionality and reliability. I designed and implemented RESTful APIs to streamline communication between frontend and backend systems, integrated real-time capabilities using WebSocket, and optimized data management with PostgreSQL and MongoDB. Additionally, I deployed applications using AWS, Google Cloud, and Kubernetes, leveraging containerization to enhance scalability and operational efficiency.",
     linkednURL: "https://www.linkedin.com/in/sikandar-hayat-381407179/",
   },
 ];
